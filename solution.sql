1c650cd7901f7f93ea8af923e02d0fd0b4fe4b89
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-22 19:23:50 +0600

    Solved query two

--- solution.sql
+++ solution.sql
@@ -64,5 +64,12 @@ VALUES
 ('Yasin', 27, 'yasin@gmail.com', 42, 52, NULL);
 
 
+-- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
+SELECT s.student_name
+FROM students s
+JOIN enrollment e ON s.student_id = e.student_id
+JOIN courses c ON e.course_id = c.course_id
+WHERE c.course_name = 'Next.js';
+
 
 
