a8b03c67d7556ac6d1775c1b18ebb5b0dcc17df8
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-22 19:45:57 +0600

    Solved query five

--- solution.sql
+++ solution.sql
@@ -91,6 +91,15 @@ WHERE course_id NOT IN (
 );
 
 
+-- Retrieve the names of students using a limit of 2, starting from the 3rd student.
+SELECT student_name
+FROM students
+ORDER BY student_id
+LIMIT 2
+OFFSET 2;
+
+
+
 
 
 
