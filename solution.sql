1f0c4aed01be34f95b4898d2c32f78aa2c018e89
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-22 19:16:08 +0600

    Solved query one

--- solution.sql
+++ solution.sql
@@ -29,3 +29,40 @@ CREATE TABLE enrollment (
 );
 
 
+-- Inserting data to student table
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status) 
+VALUES 
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+
+-- Inserting data to course table
+INSERT INTO courses (course_name, credits) 
+VALUES 
+('Next.js', 3),
+('React.js', 4),
+('Databases', 3),
+('Prisma', 3);
+
+
+-- Inserting data to enrollment table
+INSERT INTO enrollment (student_id, course_id) 
+VALUES 
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2);
+
+
+-- Insert a new student record
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status) 
+VALUES 
+('Yasin', 27, 'yasin@gmail.com', 42, 52, NULL);
+
+
+
+
